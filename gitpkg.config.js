462141e30d22485c148fcf262e8a563ad6796c7a
Harry Brundage <harry.brundage@gmail.com>
2025-10-25 16:36:09 -0400

    Expand prerelease support

--- gitpkg.config.js
+++ gitpkg.config.js
@@ -1,5 +1,9 @@
-const { execSync } = require("child_process");
+import { execSync } from "child_process";
 
-module.exports = () => ({
-  getTagName: (pkg) => `${pkg.name}-v${pkg.version}-gitpkg-${execSync("git rev-parse --short HEAD", { encoding: "utf-8" }).trim()}`,
-});
+export default () => ({
+  getTagName: (pkg) =>
+    `${pkg.name}-v${pkg.version}-gitpkg-${execSync(
+      'git rev-parse --short HEAD',
+      { encoding: 'utf-8' }
+    ).trim()}`
+})
