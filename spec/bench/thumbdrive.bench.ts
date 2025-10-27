fcd6b3709fc9a5cac73ecd518fcabd59bad179d0
Harry Brundage <harry.brundage@gmail.com>
2025-10-27 18:50:12 +0000

    Remove unused benchmark

--- spec/bench/thumbdrive.bench.ts
+++
@@ -1,9 +0,0 @@
-import { benchmarker } from "../benchmark";
-
-export default benchmarker(async (suite) => {
-  suite.add("test", function () {
-    console.log("test");
-  });
-
-  return suite;
-});
